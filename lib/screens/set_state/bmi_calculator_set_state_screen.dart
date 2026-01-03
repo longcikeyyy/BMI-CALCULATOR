cb6d2833f45408616bc96b658c7b3c82087771b7
Nobi1202 <saobang001111@gmail.com>
2026-01-03 10:40:28 +0700

    Implement initial screens and main application structure with ValueNotifier integration

---
+++ lib/screens/set_state/bmi_calculator_set_state_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BmiCalculatorSetStateScreen extends StatelessWidget {
+  const BmiCalculatorSetStateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
